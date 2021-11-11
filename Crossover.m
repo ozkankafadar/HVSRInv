0d48d072497962d760bd65fdb8edffe77e1abdf5
Dr. Özkan Kafadar <54771857+ozkankafadar@users.noreply.github.com>
2021-11-11 15:02:16 +0300

    Delete Crossover.m

--- Crossover.m
+++
@@ -1,9 +0,0 @@
-function output=Crossover(A)
-% A : Input Matrix
-    for m=1:size(A,1)/2
-        output(2*m-1,1:2:size(A,2))=A(2*m-1,1:2:size(A,2));
-        output(2*m-1,2:2:size(A,2))=A(2*m,2:2:size(A,2));
-        output(2*m,1:2:size(A,2))=A(2*m,1:2:size(A,2));
-        output(2*m,2:2:size(A,2))=A(2*m-1,2:2:size(A,2));
-    end;
-end
