b994af985efffce6dafef5e97bbc452353dd2b3a
Assoc. Prof. Dr. Özkan Kafadar <54771857+ozkankafadar@users.noreply.github.com>
2021-11-17 15:30:20 +0300

    Delete Mutation.m

--- Mutation.m
+++
@@ -1,8 +0,0 @@
-function output = Mutation(Old,P)
-% Old : Old generation
-% P   : Probability value
-    rnd=rand(size(Old));
-    mut =find(rnd<P);
-    output = Old;
-    output(mut) = 1-Old(mut);
-end
